8598752b4ed27c5617e49fcd117f7cf9d8db7b74
LocNguyenSGU <lockbkbang@gmail.com>
2024-06-30 14:34:32 +0700

    style: layout banner

--- src/App.js
+++ src/App.js
@@ -2,7 +2,27 @@
 
 function App() {
   return (
-    <><h1>HEllo</h1></>
+    <>
+      <header className="header flex items-center justify-center gap-x-5 text-white py-10">
+        <span className="text-primary">Home</span>
+        <span className="">TV Shows</span>
+        <span className="">Movies</span>
+      </header>
+      <section className="banner h-[400px] page-container">
+        <div className="w-full h-full rounded-lg relative">
+          <img className="w-full h-full object-cover rounded-lg" alt="" src="https://imagev3.vietnamplus.vn/w1000/Uploaded/2024/fsmsy/2019_04_27/avengersendgamepostercloseup.jpg.webp"></img>
+          <div className="absolute left-5 bottom-5">
+            <h2 className="text-white font-bold text-3xl mb-3">Avengers: End game</h2>
+            <div className="flex items-center gap-x-3 mb-8">
+              <span className="py-2 px-4 border border-white rounded-md text-white">Aventure</span>
+              <span className="py-2 px-4 border border-white rounded-md text-white">Action</span>
+              <span className="py-2 px-4 border border-white rounded-md text-white">Aventure</span>
+            </div>
+            <button className="py-3 px-6 rounded-lg bg-primary text-white">Watch now</button>
+          </div>
+        </div>
+      </section>
+    </>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        body: ["DM Sans", "sans-serif"],
+      },
+      colors: {
+        primary: "#F62682",
+      },
+    },
   },
   plugins: [],
-}
+};
 
