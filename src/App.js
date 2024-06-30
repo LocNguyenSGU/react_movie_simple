c61c8eac2651f080345998b737ce9933e19c37e8
LocNguyenSGU <lockbkbang@gmail.com>
2024-06-30 15:08:41 +0700

    style: card

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <span className="">TV Shows</span>
         <span className="">Movies</span>
       </header>
-      <section className="banner h-[400px] page-container">
+      <section className="banner h-[480px] page-container mb-10">
         <div className="w-full h-full rounded-lg relative">
           <img className="w-full h-full object-cover rounded-lg" alt="" src="https://imagev3.vietnamplus.vn/w1000/Uploaded/2024/fsmsy/2019_04_27/avengersendgamepostercloseup.jpg.webp"></img>
           <div className="absolute left-5 bottom-5">
@@ -19,6 +19,34 @@ function App() {
               <span className="py-2 px-4 border border-white rounded-md text-white">Aventure</span>
             </div>
             <button className="py-3 px-6 rounded-lg bg-primary text-white">Watch now</button>
+          </div>
+        </div>
+      </section>
+      <section className="movies-layout page-container mb-10">
+        <h2 className="capitalize text-white font-bold text-xl mb-4">Now playing</h2>
+        <div className="movie-list grid grid-cols-4 gap-10">
+          <div className="movie-card rounded-lg bg-slate-800 p-4">
+            <img className="w-full h-[250px] object-cover rounded-lg mb-3" src="https://imagev3.vietnamplus.vn/w1000/Uploaded/2024/fsmsy/2019_04_27/avengersendgamepostercloseup.jpg.webp" alt=""></img>
+            <h3 className="text-white font-bold text-xl mb-2">Spiderman: Home coming</h3>
+            <div className="flex items-center justify-between text-sm opacity-50 text-white mb-3">
+              <span>2017</span>
+              <span>4.5</span>
+              </div>
+              <button className="text-white bg-primary w-full font-bold rounded-lg p-3">Watch now</button>
+          </div>
+        </div>
+      </section>
+      <section className="movies-layout page-container mb-10">
+        <h2 className="capitalize text-white font-bold text-xl mb-4">Top rated</h2>
+        <div className="movie-list grid grid-cols-4 gap-10">
+          <div className="movie-card rounded-lg bg-slate-800 p-4">
+            <img className="w-full h-[250px] object-cover rounded-lg mb-3" src="https://imagev3.vietnamplus.vn/w1000/Uploaded/2024/fsmsy/2019_04_27/avengersendgamepostercloseup.jpg.webp" alt=""></img>
+            <h3 className="text-white font-bold text-xl mb-2">Spiderman: Home coming</h3>
+            <div className="flex items-center justify-between text-sm opacity-50 text-white mb-3">
+              <span>2017</span>
+              <span>4.5</span>
+              </div>
+              <button className="text-white bg-primary w-full font-bold rounded-lg p-3">Watch now</button>
           </div>
         </div>
       </section>
